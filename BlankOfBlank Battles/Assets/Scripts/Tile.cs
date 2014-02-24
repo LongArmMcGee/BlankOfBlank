9973567c548b41edcecbd30e6550cbeb8c321dc2
LongArmMcGee <ncmmjw46@gmail.com>
2014-02-24 16:58:16 -0500

    Update Tile.cs
    Class members

--- BlankOfBlank Battles/Assets/Scripts/Tile.cs
+++ BlankOfBlank Battles/Assets/Scripts/Tile.cs
@@ -2,15 +2,27 @@
 using System.Collections;
 
 public class Tile /*: MonoBehaviour DON'T NEED YET*/ { 
-
-
-
+	TileType type;
+	ArrayList<Entity> entityContainer;
+	LandEffect landEffect;
+	TileType type;
+	//used for connecting tiles via 'portals' ie tunnels/caves/magic gates
+	Tile connectedTile;
 }
 
+//Terrain cost
+enum TerrainCost{
+	FastTerrain,
+	NormalTerrain,
+	MediumTerrain,
+	HardTerrain
+}
+
+//'elemental' damage
 enum TileType{
 	Water,
 	Wood,
 	Ice,
 	Desert,
-	WhateverElseYouWantSteve,
+	WhateverElseYouWantSteve
 }
